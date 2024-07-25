b7460e035b2ff710bc5dd38b8030e64c1c17bb71
ekoida <celeno4ka@gmail.com>
2024-07-25 21:58:37 +0300

    continue with snowflake

--- src/flake/component.js
+++ src/flake/component.js
@@ -1,10 +1,12 @@
 import "./style.css";
 import { randInt, randColor } from "../helpers/generators";
 
-const Flake = ({ size = randInt(20, 40), color = randColor() }) => {
+const Flake = ({ size = randInt(20, 40), color = randColor(), top = 0, left = randInt(0, 100)}) => {
   const style = {
     "--size": `${size}px`,
     "--color": `${color}`,
+    "--top": `${top}%`,
+    '--left': `${left}%`,
   };
 
   return <div style={style} className="flake"></div>;
