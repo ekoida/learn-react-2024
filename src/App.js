2061207ec523b849e6c0b838dc9ecc2f52d35f75
ekoida <celeno4ka@gmail.com>
2024-07-26 11:29:09 +0300

    replicated flake

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Flake from "./flake/component";
+import { randInt, randColor } from "./helpers/generators";
 
 const App = () => {
   return (
     <div className="App">
-      <Flake />
+      <Flake
+        size={randInt(20, 40)}
+        color = {randColor()}
+        top = {0}
+        left = {randInt(0, 100)}
+      />
     </div>
   );
 };
--- src/flake/component.js
+++ src/flake/component.js
@@ -1,14 +1,19 @@
 import "./style.css";
-import { randInt, randColor } from "../helpers/generators";
+import { useState } from "react";
 
-const Flake = ({ size = randInt(20, 40), color = randColor(), top = 0, left = randInt(0, 100)}) => {
+//HW1 - make sure the component detects the moment it reahces the bottom
+//HW2 - when the component reaches the botom it should remove it self form the virtual DOM
+const Flake = ({size = 0, color = 0, top = 0, left = 0}) => {
+  const [_top, _setTop ] = useState(top)
 
-  
+  setTimeout(() => {
+    _setTop(_top + 10)
+  }, 500)
   
   const style = {
     "--size": `${size}px`,
     "--color": `${color}`,
-    "--top": `${top}%`,
+    "--top": `${_top}%`,
     '--left': `${left}%`,
   };
 
