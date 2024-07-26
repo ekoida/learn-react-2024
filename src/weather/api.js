0a479e675ebf96240fdd105b7aab90e02035ba32
ekoida <celeno4ka@gmail.com>
2024-07-26 18:46:45 +0300

    added basic forecast

--- src/weather/api.js
+++ src/weather/api.js
@@ -1,32 +1,16 @@
-const token = 'get token from the .env file'
+export const getForecast = async (position) => {
+    console.log(position)
+    const {coords: {latitude, longitude}} = position
 
-export const getLatestForecast = () => {
-    if(!window.navigator.geolocation) {
-        alert("geolocation is not enabled")
-        return
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=13f21668a6b69571d63bc59c79d69966&units=metric`)
+    if(!response.ok) {
+        return Promise.reject(response)
     }
-
-    const success = async (position) => {
-        const {lattitude: lat, longitude: lon} = position
-
-        try {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={API key}`)
-        if(!response.ok) {
-            return Promise.reject(response)
-        }
-        const data = await response.json()
-        console.log(data)
-        } catch (error) {
-            console.log('in catch  block')
-            console.log(error)
-        }
-        
-    }
-
-    const error = (error) => {
-        console.log('in error callback')
+   return response.json()
+   
+    } catch (error) {
+        console.log('in catch  block')
         console.log(error)
-    }
-    
-    navigator.geolocation.getCurrentPosition(success, error)
+    } 
 }
--- src/weather/weather.jsx
+++ src/weather/weather.jsx
@@ -1,11 +1,33 @@
-import { getLatestForecast } from './api';
+import { useEffect, useState } from 'react';
+import {getForecast } from './api';
 import './styles.scss';
 
 export const Weather = () => {
+   const [data, setData] = useState(null)
+
+    useEffect(() => {
+        if(!window.navigator.geolocation) {
+            alert("geolocation is not enabled")
+            return
+        }
+        navigator.geolocation.getCurrentPosition((position) => {
+            getForecast(position)
+            .then(response => {
+                setData(response)
+            })
+            .catch(error => console.log(error))
+            .finally(() => {
+                console.log(data)
+            })
+        },
+         (error) => {
+            console.log(error)
+        })
+    }, [])
 
     return (
         <div className='weather'>
-            Hello weather
+            <p>Hello weather</p>
         </div>
     )
 }
