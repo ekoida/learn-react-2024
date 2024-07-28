e4fc3f2d049502f4328f0f1557ec999a7a8af106
ekoida <celeno4ka@gmail.com>
2024-07-28 11:07:09 +0300

    added url search params and updated api

--- src/weather/api.js
+++ src/weather/api.js
@@ -1,9 +1,18 @@
+const HOST = 'https://api.openweathermap.org/data/2.5/weather'
+const KEY = '13f21668a6b69571d63bc59c79d69966'
+
 export const getForecast = async (position) => {
     console.log(position)
     const {coords: {latitude, longitude}} = position
 
+    const params = new URLSearchParams({
+        lat: latitude,
+        lon: longitude,
+        units: 'metric',
+        appid: KEY
+    })
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=13f21668a6b69571d63bc59c79d69966&units=metric`)
+        const response = await fetch(`${HOST}?${params}`)
     if(!response.ok) {
         return Promise.reject(response)
     }
