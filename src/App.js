5106cdade2bd3273804404727b7c3d6e07617d4b
ekoida <celeno4ka@gmail.com>
2024-07-31 18:15:34 +0300

    conspected flake

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import Flake from "./flake/component";
 import { randInt, randColor } from "./helpers/generators";
 
 const App = () => {
+  let [showChild, setShowChild] = useState(true);
+
   return (
     <div className="App">
-      <Flake
-        size={randInt(20, 40)}
-        color = {randColor()}
-        top = {0}
-        left = {randInt(0, 100)}
-      />
+      {showChild && (
+        <Flake
+          size={randInt(20, 40)}
+          color={randColor()}
+          top={0}
+          left={randInt(0, 100)}
+          whenDone={() => {
+            setShowChild(false);
+          }}
+        />
+      )}
     </div>
   );
 };
