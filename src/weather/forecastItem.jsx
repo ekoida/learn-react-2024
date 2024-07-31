cd01ef013eba77161db6d8945f4456f5b2fe1348
ekoida <celeno4ka@gmail.com>
2024-07-31 13:22:49 +0300

    formatted code

--- src/weather/forecastItem.jsx
+++ src/weather/forecastItem.jsx
@@ -1,19 +1,21 @@
-import { ICON_HOST } from "./api"
-export const ForecastItem = ({forecastItem}) => {
-    return (
-        <li className="forecast-item">
-            <p>Date: <span>{forecastItem.dt_txt}</span></p>
-                    <p>
-                        Teperature:
-                         <span>{forecastItem.main.temp}</span>
-                         {/* to render icons we have to call cpecial api endpoint on open weather provider and apply the url */}
-                         {/* if we want different size of the icon we can change the ratio after @ sign */}
-                        <img
-                            src={`${ICON_HOST}/${forecastItem.weather[0].icon}@2x.png`}
-                            alt={forecastItem.weather[0].description}
-                            title={forecastItem.weather[0].description}
-                        />
-                    </p>
-        </li>
-    )
-}
+import { ICON_HOST } from "./api";
+export const ForecastItem = ({ forecastItem }) => {
+  return (
+    <li className="forecast-item">
+      <p>
+        Date: <span>{forecastItem.dt_txt}</span>
+      </p>
+      <p>
+        Teperature:
+        <span>{forecastItem.main.temp}</span>
+        {/* to render icons we have to call cpecial api endpoint on open weather provider and apply the url */}
+        {/* if we want different size of the icon we can change the ratio after @ sign */}
+        <img
+          src={`${ICON_HOST}/${forecastItem.weather[0].icon}@2x.png`}
+          alt={forecastItem.weather[0].description}
+          title={forecastItem.weather[0].description}
+        />
+      </p>
+    </li>
+  );
+};
--- src/weather/weather.jsx
+++ src/weather/weather.jsx
@@ -1,53 +1,62 @@
-import { useEffect, useState } from 'react';
-import {getForecast, ICON_HOST } from './api';
-import {ReactComponent as IconRight} from '../icons/arrow-right-circle.svg'
-import { Forecast } from './forecast';
-import './styles.scss';
+import { useEffect, useState } from "react";
+import { getForecast, ICON_HOST } from "./api";
+import { ReactComponent as IconRight } from "../icons/arrow-right-circle.svg";
+import { Forecast } from "./forecast";
+import "./styles.scss";
 
 export const Weather = () => {
-   const [data, setData] = useState(null)
-   const [showMore, setShowMore] = useState(false)
+  const [data, setData] = useState(null);
+  const [showMore, setShowMore] = useState(false);
 
-    useEffect(() => {
-        if(!window.navigator.geolocation) {
-            alert("geolocation is not enabled")
-            return
-        }
-        navigator.geolocation.getCurrentPosition((position) => {
-            getForecast(position)
-            .then(response => {
-                setData(response)
-            })
-            .catch(error => console.log(error))
-        },
-         (error) => {
-            console.log(error)
-        })
-    }, [])
-
-
-    // this method works as simmple true/false switcher
-    // if showMore state is false it will converti it to true and vice versa
-    const toggleForecast = () => {
-        setShowMore(state => !state)
+  useEffect(() => {
+    if (!window.navigator.geolocation) {
+      alert("geolocation is not enabled");
+      return;
     }
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        getForecast(position)
+          .then((response) => {
+            setData(response);
+          })
+          .catch((error) => console.log(error));
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
 
-    return (
-        <div className='weather'>
-            <p>
-                {data?.name}
-                {/* show icon using the api for icons from openweather */}
-                <img src={`${ICON_HOST}${data?.weather[0].icon}.png`} title={data?.weather[0].description} alt={data?.weather[0].description} />
-            </p>
-            <p>temperature: {data?.main.temp}</p>
-            <p>clouds: {data?.clouds.all}</p>
-            <button 
-                className='button-more'
-                onClick={toggleForecast}>
-                    See Forecast <IconRight className='icon' />
-            </button>
-            {/* render Forecast component if the showMore is true */}
-            {showMore && <Forecast/>}
-        </div>
-    )
-}
+  // this method works as simmple true/false switcher
+  // if showMore state is false it will converti it to true and vice versa
+  const toggleForecast = () => {
+    setShowMore((state) => {
+      if (state === true) {
+        state = false;
+      } else {
+        state = true;
+      }
+    });
+  };
+
+  return (
+    <div className="weather">
+      <p>
+        {data?.name}
+        {/* show icon using the api for icons from openweather */}
+        <img
+          src={`${ICON_HOST}${data?.weather[0].icon}.png`}
+          title={data?.weather[0].description}
+          alt={data?.weather[0].description}
+        />
+      </p>
+      <p>temperature: {data?.main.temp}</p>
+      <p>clouds: {data?.clouds.all}</p>
+      <button className="button-more" onClick={toggleForecast}>
+        See Forecast <IconRight className="icon" />
+      </button>
+      {/* render Forecast component if the showMore is true */}
+      {showMore && <Forecast />}
+    </div>
+  );
+};
